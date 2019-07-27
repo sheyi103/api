a7ff42594920d1a1c524630d314001a127f7378d
seyi olawepo <sheyi103@outlook.com>
2019-07-27 21:26:57 +0100

    Relationship created

--- app/model/Product.php
+++ app/model/Product.php
@@ -2,9 +2,13 @@
 
 namespace App\model;
 
+use App\model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews(){
+
+        return $this->hasMany(Review::class);
+    }
 }
--- app/model/Review.php
+++ app/model/Review.php
@@ -2,9 +2,13 @@
 
 namespace App\model;
 
+use App\model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product(){
+
+        return $this->belongsTo(Product::class);
+    }
 }
